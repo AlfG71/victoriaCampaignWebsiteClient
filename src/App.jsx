d92f09303c3b8997218f2b8ae9c6abaa3e0c1046
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2023-11-26 22:14:05 -0400

    styling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,19 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import BackGroundIMG from './assets/Fotos_para_pagina_de_Myrna/victor-e015e487be9999636f9ea6350ff28a8a-1200x800-876085745.jpeg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div class="bg-blue-500 bg-opacity-75" className="App">
+    <div className="bg-blue-500 bg-opacity-75 App full-height">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+
+      <div className="flex-1 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${BackGroundIMG})` }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+
       <Footer />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom";
 import HomeImage from "../assets/Fotos_para_pagina_de_Myrna/IMG_7976 Medium.jpeg"
 import FotoCandidata from "../assets/Fotos_para_pagina_de_Myrna/IMG_7966 Medium.jpeg";
+import FotoPerfil1 from "../assets/Fotos_para_pagina_de_Myrna/Foto MC 2x2 para papeleta. MVC.jpeg";
 
-// /assets/Fotos_para_pagina_de_Myrna/MVC-877874243.png
 const Home = () => {
   return (
-    <div id="home" className="bg-[url('src/assets/Fotos_para_pagina_de_Myrna/victor-e015e487be9999636f9ea6350ff28a8a-1200x800-876085745.jpeg')] bg-cover bg-center min-h-screen">
+    <div id="home">
 
-    {/* <img src={ HomeImage } alt="Foto de la candidata" className="rounded-full items-center justify-center relative w-22 h-22 top-1/2 left-1/3"/> */}
-    {/* <img src={ FotoCandidata } class="inline" alt="Foto de la candidata"/> */}
+      <img src={FotoPerfil1} className="rounded-full home-image"/>
 
     </div>
   )
